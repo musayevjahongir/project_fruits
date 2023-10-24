3aa57493909b1959a5d38aacf6c8f6070143761e
musayevjahongir <musayevjahongir5051@gmail.com>
2023-10-24 10:21:39 +0500

    eng qimmat narx

--- get_cheapest_fruit.py
+++ get_cheapest_fruit.py
@@ -7,5 +7,15 @@ def get_cheapest_fruit(data: str) -> str:
     returns:
         str: name of the cheapest fruit
     """
-    # your code here
-    pass
+    from csv import reader
+    f=open(data)
+    f=reader(f)
+    a=[]
+    b=[]
+    for i in f:
+        a.append(i[1])
+    a=a[1:]
+    for i in a:
+        b.append(float (i))
+    return max(b)
+print(get_cheapest_fruit("fruits.csv"))
