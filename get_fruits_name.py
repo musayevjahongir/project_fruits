009a76ded36153f816f10ca84351651b832cbb23
musayevjahongir <musayevjahongir5051@gmail.com>
2023-10-25 10:38:49 +0500

    mevalar ro'rxati

--- get_fruits_name.py
+++ get_fruits_name.py
@@ -7,6 +7,16 @@ def get_frutis_name(data:str)->list:
     returns:
         list: list of fruits names
     """
-    return
+    a=[]
+    b=[]
+    from csv import reader
+    f=open(data)
+    f=reader(f)
+    for i in f:
+        a.append(i)
+    for i in a:
+        b.append(i[0])
+    return b[1:]
+print(get_frutis_name("fruits.csv"))
 
     
