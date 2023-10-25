e102eba5265b536b34cebd397caf3b679257c357
musayevjahongir <musayevjahongir5051@gmail.com>
2023-10-25 10:45:41 +0500

    narxlar yig'indisi

--- get_total_price.py
+++ get_total_price.py
@@ -7,6 +7,16 @@ def get_total_price(data:str)->float:
     returns:
         float: fruits total price
     """
-    return
+    a=[]
+    s=0
+    from csv import reader
+    f=open(data)
+    f=reader(f)
+    for i in f:
+        a.append(i)
+    for i in a[1:]:
+        s+=float(i[1])
+    return s
+print(get_total_price("fruits.csv"))
 
     
